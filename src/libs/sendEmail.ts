bdf76873f758324298b84c95211fed6f3abd3b40
matpiovesan16 <101372551+matpiovesan16@users.noreply.github.com>
2025-07-16 13:24:48 -0300

    reset contraseña funcionando

---
+++ src/libs/sendEmail.ts
@@ -0,0 +1,27 @@
+import nodemailer from "nodemailer";
+
+export async function sendEmail({
+  to,
+  subject,
+  html,
+}: {
+  to: string;
+  subject: string;
+  html: string;
+}) {
+  const transporter = nodemailer.createTransport({
+    service: "gmail", // o "hotmail", etc., según uses
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  return transporter.sendMail({
+    from: `"Soporte Trivo" <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    html,
+  });
+}
+
