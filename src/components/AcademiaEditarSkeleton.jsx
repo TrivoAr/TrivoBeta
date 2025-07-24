3ce78b117a662795b17a456842ecd702825a3099
matpiovesan16 <101372551+matpiovesan16@users.noreply.github.com>
2025-07-23 21:03:27 -0300

    skeleton agregado para academia

---
+++ src/components/AcademiaEditarSkeleton.jsx
@@ -0,0 +1,48 @@
+import Skeleton from 'react-loading-skeleton';
+import React from 'react';
+
+const baseColor = '#e0e0e0';
+const highlightColor = '#f5f5f5';
+
+const AcademiaEditarSkeleton = () => {
+  return (
+    <div className="w-[390px] flex flex-col items-center gap-5">
+      {/* Header y botón volver */}
+      <div className="relative w-full h-[30px] flex">
+        <div className="absolute top-2 left-2">
+          <Skeleton circle width={36} height={36} baseColor={baseColor} highlightColor={highlightColor} />
+        </div>
+      </div>
+      <Skeleton width={180} height={28} baseColor={baseColor} highlightColor={highlightColor} className="mb-2" />
+      <div className="w-[390px] mx-auto p-4 space-y-5 rounded-xl mb-[80px] bg-[#FEFBF9]">
+        {/* Input nombre_academia */}
+        <Skeleton className="w-full px-4 py-6 rounded-[40px] my-5" baseColor={baseColor} highlightColor={highlightColor} />
+        {/* Input precio */}
+        <Skeleton className="w-full px-4 py-6 rounded-[40px] my-5" baseColor={baseColor} highlightColor={highlightColor} />
+        {/* Select país */}
+        <Skeleton className="w-full px-4 py-6 rounded-[40px] my-5" baseColor={baseColor} highlightColor={highlightColor} />
+        {/* Select provincia */}
+        <Skeleton className="w-full px-4 py-6 rounded-[40px] my-5" baseColor={baseColor} highlightColor={highlightColor} />
+        {/* Select localidad */}
+        <Skeleton className="w-full px-4 py-5 rounded-[40px] my-5" baseColor={baseColor} highlightColor={highlightColor} />
+        {/* Textarea descripción */}
+        <Skeleton className="w-full px-4 py-9 rounded-[40px] my-5" baseColor={baseColor} highlightColor={highlightColor} />
+        {/* Select tipo_disciplina */}
+        <Skeleton className="w-full px-4 py-4 rounded-[40px] my-5" baseColor={baseColor} highlightColor={highlightColor} />
+        {/* Checkbox clase gratis */}
+        <div className="flex items-center gap-2 my-5">
+          <Skeleton circle width={24} height={24} baseColor={baseColor} highlightColor={highlightColor} />
+          <Skeleton width={120} height={18} baseColor={baseColor} highlightColor={highlightColor} />
+        </div>
+        {/* Imagen */}
+        <Skeleton className="w-full h-48 rounded-xl my-5" baseColor={baseColor} highlightColor={highlightColor} />
+        {/* Botón guardar */}
+        <Skeleton className="w-full h-12 rounded-[20px] my-5" baseColor={baseColor} highlightColor={highlightColor} />
+        {/* Botón eliminar */}
+        <Skeleton className="w-full h-10 rounded-[10px] my-5" baseColor={baseColor} highlightColor={highlightColor} />
+      </div>
+    </div>
+  );
+};
+
+export default AcademiaEditarSkeleton; 
