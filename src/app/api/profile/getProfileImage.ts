5bf1ba57dd32ca4030b17e3f8b12531127945c91
matpiovesan16 <101372551+matpiovesan16@users.noreply.github.com>
2025-08-10 19:55:24 -0300

    cambios

--- src/app/api/profile/getProfileImage.ts
+++ src/app/api/profile/getProfileImage.ts
@@ -1,5 +1,5 @@
 // api/profile/getProfileImage.ts
-  import { ref, getDownloadURL } from "firebase/storage";
+import { ref, getDownloadURL } from "firebase/storage";
 import { storage } from "@/libs/firebaseConfig";
 
 export const getProfileImage = async (fileName: string, userId: string): Promise<string> => {
