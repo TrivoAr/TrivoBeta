d8c5afbaa6b6dd58130071182d27ce42c01a93fb
matpiovesan16 <101372551+matpiovesan16@users.noreply.github.com>
2025-08-10 20:58:09 -0300

    cambios para vercel parte mil

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -13,34 +13,43 @@ interface UserProfileProps {
     joinDate: string;
   };
 }
-
-
-
-
-
-
-
-export default async function UserProfile({ user }: UserProfileProps) {
-  const router = useRouter();
-    
-  return (
-  
-  <div className="w-[390px]">
-    <div className="">
-         <button
-        onClick={() => router.back()}
-        className="text-[#C76C01] self-star shadow-md rounded-full w-[40px] h-[40px] flex justify-center items-center ml-5 mt-5 mb-2"
-      >
-        <img
-          src="/assets/icons/Collapse Arrow.svg"
-          alt="callback"
-          className="h-[20px] w-[20px]"
-        />
-      </button>
-    </div>
-     
-      <UserPublicProfile/>
-  </div>
-  )
+ type PageProps = { params: { id: string } };
+ export default async function ProfilePage({ params }: PageProps) {
+  const { id } = params;
+  return <UserPublicProfile userId={id} />;
 }
+
+
+
+
+
+
+
+// export default async function UserProfile({ user }: UserProfileProps) {
+//   const router = useRouter();
+
+ 
+
+
+    
+//   return (
+  
+//   <div className="w-[390px]">
+//     <div className="">
+//          <button
+//         onClick={() => router.back()}
+//         className="text-[#C76C01] self-star shadow-md rounded-full w-[40px] h-[40px] flex justify-center items-center ml-5 mt-5 mb-2"
+//       >
+//         <img
+//           src="/assets/icons/Collapse Arrow.svg"
+//           alt="callback"
+//           className="h-[20px] w-[20px]"
+//         />
+//       </button>
+//     </div>
+     
+//       <UserPublicProfile/>
+//   </div>
+//   )
+// }
 
